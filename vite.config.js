01e41fb6785ee7e4d057c0f121064ab24e8d7357
kkrot17-hub <kkrot17@gmail.com>
2025-12-24 19:25:01 +0100

    2 Fix comment formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/hotel-dashboard/',  // <- مهم جدًا
+  base: '/hotel-dashboard/',  // ← مهم جدًا
   plugins: [react()],
 });
