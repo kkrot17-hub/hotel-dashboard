812507d6fff300bd4703eaaaf43fd21e796f728d
kkrot17-hub <kkrot17@gmail.com>
2025-12-24 20:15:20 +0100

    Remove unused imports from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+ 
 export default defineConfig({
-  base: '/hotel-dashboard/',  // ← اسم المستودع
+  base: "/hotel-dashboard/",
   plugins: [react()],
 });
